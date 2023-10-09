307ade20c59c2afc31a1b18cc24f5b40d1be1949
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2023-10-09 14:42:17 -0300

    Updating function to call the remote method

--- velha-socket-final-client/src/main/java/network/handler/SendMessageHandler.java
+++ velha-socket-final-client/src/main/java/network/handler/SendMessageHandler.java
@@ -1,17 +1,12 @@
 package network.handler;
 
 import model.Message;
+import network.configuration.Client;
 
 import static model.enumeration.MessageCode.Code.*;
 
-/**
- * Interacts with the DataFlowHandler class to send messages
- */
 public class SendMessageHandler {
-
-    private final DataFlowHandler dataFlowHandler = DataFlowHandler.getInstance();
-
-    public static SendMessageHandler sendMessageHandler;
+    private static SendMessageHandler sendMessageHandler;
 
     public static synchronized SendMessageHandler getInstance() {
         if (sendMessageHandler == null) {
@@ -22,12 +17,49 @@ public class SendMessageHandler {
 
     public void setMessageToSend(Message message) {
         switch (message.getCode()) {
-            case GIVE_UP -> dataFlowHandler.setMessageToSend( GIVE_UP.code);
-            case PASS_TURN -> dataFlowHandler.setMessageToSend(PASS_TURN.code + message.getTextMessage());
-            case CLOSE_WINDOW -> dataFlowHandler.setMessageToSend(CLOSE_WINDOW.code);
-            case INIT_NUMBER -> dataFlowHandler.setMessageToSend(INIT_NUMBER.code + message.getTextMessage());
-            case CHAT -> dataFlowHandler.setMessageToSend(CHAT.code + message.getTextMessage());
-            case EMPTY -> dataFlowHandler.setMessageToSend(EMPTY.code);
+
+            case GIVE_UP -> {
+                try {
+                    Client.remoteObject.sendMessage(GIVE_UP.code + message.getTextMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            case PASS_TURN -> {
+                try {
+                    Client.remoteObject.sendMessage(PASS_TURN.code + message.getTextMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            case CLOSE_WINDOW -> {
+                try {
+                    Client.remoteObject.sendMessage(CLOSE_WINDOW.code);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            case INIT_NUMBER -> {
+                try {
+                    Client.remoteObject.sendMessage(INIT_NUMBER.code + message.getTextMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            case CHAT -> {
+                try {
+                    Client.remoteObject.sendMessage(CHAT.code + message.getTextMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            case EMPTY -> {
+                try {
+                    Client.remoteObject.sendMessage(EMPTY.code + message.getTextMessage());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
     }
 }
