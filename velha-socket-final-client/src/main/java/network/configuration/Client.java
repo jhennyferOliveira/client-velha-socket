2e91747b7d5eff934cfe73f8630523618c074feb
Jhennyfer Oliveira <jhennyfer519@gmail.com>
2023-10-09 14:40:41 -0300

    Updating Client to use RMI

--- velha-socket-final-client/src/main/java/network/configuration/Client.java
+++ velha-socket-final-client/src/main/java/network/configuration/Client.java
@@ -1,37 +1,25 @@
 package network.configuration;
 
-import network.handler.DataFlowHandler;
+import network.handler.ReceivedMessageHandler;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
+import java.rmi.RemoteException;
 
-public class Client extends Thread {
-    private final DataFlowHandler dataFlowHandler = DataFlowHandler.getInstance();
-    private Socket socket;
 
-    public void clientSocketConfiguration() {
-        try {
-            var port = 9090;
-            var host = "localhost";
-            this.socket = new Socket(host, port);
-            System.out.println("Conectado....");
-            this.start();
-            var outputStream = new DataOutputStream(socket.getOutputStream());
-            dataFlowHandler.sendMessage(outputStream);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+public class Client implements DataHandlerInterface {
+
+    public Client() throws RemoteException {
+        super();
+
     }
+    public static DataHandlerInterface remoteObject;
+    private final ReceivedMessageHandler receivedMessageHandler = new ReceivedMessageHandler();
 
-    public void run() {
-        DataInputStream inputStream;
-        try {
-            inputStream = new DataInputStream(socket.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        dataFlowHandler.receiveMessage(inputStream);
+
+    /**
+     * Message received from the server
+     */
+    @Override
+    public void sendMessage(String message) throws RemoteException {
+        receivedMessageHandler.callService(message);
     }
 }
